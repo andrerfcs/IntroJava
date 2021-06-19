d0401286f751d274117d2ed81753064419ec2a5b
André Ricardo <andrerfcs@gmail.com>
2021-06-19 09:16:05 -0300

    (1) - Desenho do quadrado (5) - Tabuada (6) - Fibonacci (7) - Contagem Regressiva (s) - Sair

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -1,29 +1,121 @@
 // 1 - Pacote
-package intro;
+package fundamentos;
 
-// 2 - Referência as bibliotecas
+// 2 - Bibliotecas
+import java.util.Scanner;
 
 // 3 - Classe
 public class Medidas {
-    // 3.1 - Atributos - Caracteristicas
+    // 3.1 - Atributos
+    static Scanner entrada = new Scanner(System.in);
 
     // 3.2 - Métodos e Funções
-    public static void main(String[] args){
-        System.out.println("Meu primeiro codigo em Java!!!");
-        System.out.println("Bom dia!!!");
+    public static void main(String[] args) {
+        String opcao = "";
+        int area = 0; // receber o resultado dos cálculos de áreas
 
+        while (!opcao.equals("S") && !opcao.equals("s")) {
 
-        // Calculo de área - Exemplo: o tamanho do tapete ou do piso
-        int largura;
-        int comprimento;
-        int resultado;
+            System.out.println("Escolha o Calculo Desejado");
+            System.out.println("(1) - Area do Quadrado");
+            System.out.println("(2) - Area do Retangulo");
+            System.out.println("(3) - Area do Triangulo");
+            System.out.println("(4) - Area do Circulo");
+            System.out.println("(5) - Tabuada");
+            System.out.println("(6) - Fibonacci");
+            System.out.println("(7) - Contagem Regressiva");
+            System.out.println("(S) - Sair");
 
-        largura = 5; // largura recebe 4
-        comprimento = 6;
+            opcao = entrada.nextLine(); // leitura da opção
+            switch (opcao) {
+                case "1":
+                    area = calcularAreaDoQuadrado();
+                    break;
+                case "2":
+                    // ToDo: calcular área do retangulo
+                    break;
+                case "5":
+                    tabuada();
+                    break;
+                case "6":
+                    fibonacci();
+                    break;
+                case "7":
+                    contagemRegressiva();
+                    break;
+                default:
+                    System.out.println("Opcao Invalida: " + opcao);
+            }
+            if (area > 0) {
+                System.out.println("A área é de " + area + "m²");
+            }
+        }
+        System.out.println("Agradecemos pela preferencia! Fui!!");
+    }
 
-        resultado = largura * comprimento;
-        System.out.println("Para a largura de " + largura + "m e o comprimento de "
-                            + comprimento + "m a área é de " + resultado + "m²");
+    public static int calcularAreaDoQuadrado(){
+
+        int lado; // tamanho do lado do quadrado
+
+        System.out.println("Digite o tamanho do lado: ");
+        lado = entrada.nextInt(); // leitura do tamanho do lado
+
+        // Desenhar o quadrado
+        for(int linha = 1; linha <= lado; linha++) {
+            for(int coluna = 1; coluna <= lado; coluna++ ) {
+                System.out.print(" # ");
+            }
+            System.out.println(""); //pular de linha
+        }
+        System.out.println(""); // pular de linha
+        return lado * lado; // retorna a área do quadrado
 
     }
+    public static void tabuada(){
+        System.out.print("Você quer calcular a tabuada de qual numero? ");
+        byte numero = entrada.nextByte();
+
+        for (byte i=1;i<=10;i++){
+            System.out.print(numero * i + " ");
+        }
+    }
+
+    public static void fibonacci(){
+        System.out.print("Quantos números deseja calcular na sequencia? ");
+        byte numero = entrada.nextByte();
+
+        switch(numero){
+            case 0:
+                System.out.println("A sequencia está vazia.");
+                break;
+            case 1:
+                System.out.println("Sequencia de Fibonacci: 1");
+                break;
+            default:
+                int num1 = 0;
+                int num2 = 1;
+
+                System.out.print("Sequencia de Fibonacci: 1 ");
+                for(byte i = 2; i <= numero; i++ ){
+                    int fib = num1 + num2;
+                    System.out.print(fib + " ");
+                    num1 = num2;
+                    num2 = fib;
+                }
+        }
+    }
+
+    public static void contagemRegressiva(){
+
+        System.out.println("Comecar a contagem a partir de qual numero? ");
+        int numero = entrada.nextInt();
+
+        System.out.println("Diminuindo de quantos em quantos numeros por vez? ");
+        int decresimo = entrada.nextInt();
+
+        for(int i = numero; i >= 1; i-= decresimo){
+            System.out.println(i);
+        }
+    }
+
 }
